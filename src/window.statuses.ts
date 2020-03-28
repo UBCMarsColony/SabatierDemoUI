2f2c22306a8ea7000086c7b6f0ecb681499b09c3
Thomas Richmond <thomas.joakim@gmail.com>
2020-03-28 16:26:15 -0700

    Refactored a ton of code to (a) reduce clutter in renderer, (b) put functions and costructs in more logical locations and (c) clean up code flow

--- src/window.statuses.ts
+++ src/window.statuses.ts
@@ -1,28 +1,34 @@
-enum Status {
-	UNKNOWN =		'Unknown',		// Only during startup
-	SCANNING =		'Scanning',		// Looking for a serial connection
-	ATTACHED = 		'Attached',		// Ready to connect to serial
-	CONNECTED = 	'Connected',	// Connected to serial	
-	DISCONNECTED = 	'Disconnected'	// Serial connection unexpectedly closed; attempting recovery
-};
-export const UNKNOWN = 		Status.UNKNOWN;
-export const SCANNING = 	Status.SCANNING;
-export const ATTACHED = 	Status.ATTACHED;
-export const CONNECTED = 	Status.CONNECTED;
-export const DISCONNECTED = Status.DISCONNECTED;
+import { SerialStatus } from "./serial"
 
-export function update(status: Status)
+// Lookup table for status strings
+type StatusGData = {  // Graphical Data lookup for display purposes
+	str: string,
+	color: string,
+	interactive: boolean}
+const status_gdata_lookup: Array<StatusGData> = [];
+status_gdata_lookup[SerialStatus.UNDETERMINED] =
+	{str: "Undetermined", color: "black", interactive: false};
+status_gdata_lookup[SerialStatus.SCANNING]     = 
+	{str: "Scanning    ", color: "blue",  interactive: false};
+status_gdata_lookup[SerialStatus.ATTACHED]     = 
+	{str: "Attached    ", color: "blue",  interactive: true};
+status_gdata_lookup[SerialStatus.CONNECTED]    = 
+	{str: "Connected   ", color: "green", interactive: true};
+status_gdata_lookup[SerialStatus.DISCONNECTED] = 
+	{str: "Disconnected", color: "red",   interactive: false};
+console.log(status_gdata_lookup);
+export function update(serial_status: SerialStatus)
 {
-	document.getElementById('reactor-status').style.color =
-		status === Status.CONNECTED 		? 'green' 	:
-		status === Status.DISCONNECTED 		? 'red'   	:
-		/*  Default  */						  'black'	;
-	document.getElementById('reactor-status').innerHTML = status;
+	let status = status_gdata_lookup[serial_status]
+	document.getElementById('reactor-status').style.color = status.color;
+	document.getElementById('reactor-status').innerHTML = status.str;
+	const inter_button = document.  // The connection button 
+		getElementById('connect-toggle-button') as HTMLButtonElement;
 	
-	const connect_toggle_button = document.getElementById('connect-toggle-button') as HTMLButtonElement;
-	connect_toggle_button.disabled = (status === UNKNOWN || status === SCANNING);
-	if (status !== ATTACHED && status !== DISCONNECTED)
-		connect_toggle_button.innerHTML = "DISCONNECT"; 
+	inter_button.disabled = !status.interactive;
+	if (serial_status == SerialStatus.ATTACHED)
+		inter_button.innerHTML = "CONNECT"; 
 	else
-		connect_toggle_button.innerHTML = "CONNECT";
+		inter_button.innerHTML = "DISCONNECT";
 }
+
